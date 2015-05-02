51ba8722986c8bb8ad02369c98d857dbd0658f41
Ray Farias <ray@rayfarias.com>
2015-05-01 23:58:19 -1000

    add work description stubs

---
+++ calculator.js
@@ -0,0 +1,62 @@
+/**
+ * Declare a function named `makeCalculator`
+ * this function will have two private variables declared inside of it.
+ * @variable PRIVATE { Number } `memory`
+ * @variable PRIVATE { Number } `total`
+ * @return {object} `calculator` object that can be used
+ */
+
+
+
+  /**
+   * sets the `total` to the number passed in
+   * @param  { Number } x
+   * @return { Number }    current total
+   */
+
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+
+
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+
+
+  /**
+   * Clear the value stored at `memory`
+   */
+
