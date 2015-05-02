f4a3d3cace0ed78f8c334863a2592b276978e882
Ray Farias <ray@rayfarias.com>
2015-05-01 18:38:42 -1000

    initial commit; base exercise structure and settings

---
+++ test/test-script.js
@@ -0,0 +1,44 @@
+/****Check if Browser or CLI****/
+var window = window || undefined;
+
+if (window) {
+  GLOBAL = window;
+} else {
+  var fs = require('fs');
+  var vm = require('vm');
+  var sinon = require('sinon');
+  var chai = require('chai');
+  var functionsFile = fs.readFileSync(process.cwd() + '/calculator.js');
+  vm.runInThisContext(functionsFile); // file runs and it's contents has access to GLOBAL
+}
+/*******************************/
+var expect = chai.expect;
+var should = chai.should();
+
+var calculator = GLOBAL.makeCalculator;
+
+describe("Calculator", function() {
+  var sandbox;
+
+  beforeEach(function() {
+    // create a sandbox
+    sandbox = sinon.sandbox.create();
+
+    // stub some console methods
+    sandbox.stub(console, "log");
+    sandbox.stub(console, "error");
+  });
+
+  afterEach(function() {
+    // restore the environment as it was before
+    sandbox.restore();
+  });
+
+  describe('#makeCalculator', function () {
+    it('should be a function', function () {
+      expect(calculator).to.exist;
+      expect(calculator).to.be.an('function');
+    });
+  });
+
+});
