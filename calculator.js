e7c22008445f2720ecef4988019a8d738cdbfc38
thingsastray <andrewpeterson808@gmail.com>
2015-10-31 15:09:37 -1000

    v1 complete

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+// var myCalculator = calculatorModule(); // creates a new calculator to be used
+//     myCalculator.load(6); // initial value to be stored in a private variable named `total`
+//     myCalculator.add(5); // adds 5 to `total`
+//     myCalculator.getTotal(); // returns the value of `total`
+
 /**
  * Declare a function named `calculatorModule`
  * this function will have two private variables declared inside of it.
@@ -5,61 +10,106 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
-
-
+ 
+function calculatorModule() {
+      var memory = 0;
+      var total = 0;
+      var calculator = {
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+      load: function(x){
+          if(typeof x !== 'number'){
+            return 'Not a vaild input';
+          }
+        total = x;
+        return x;
+      },
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+      getTotal: function(){
+        return total;
+      },
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+      add: function(x){
+        if(typeof x !== 'number'){
+            return 'Not a vaild input';
+          }
+        total += x;
+        return total;
+      },
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+      subtract: function(x){
+        if(typeof x !== 'number'){
+            return 'Not a vaild input';
+          }
+        total -= x;
+        return total;
+      },
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+      multiply: function(x){
+        if(typeof x !== 'number'){
+            return 'Not a vaild input';
+          }
+        total *= x;
+        return total;
+      },
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+      divide: function(x){
+        if(typeof x !== 'number'){
+            return 'Not a vaild input';
+          }
+        total /= x;
+        return total;
+      },
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+      recallMemory: function(x){
+        return memory;
+        },
 
   /**
    * Stores the value of `total` to `memory`
    */
-
-
+      saveMemory: function(){
+        memory = total;
+      },
   /**
    * Clear the value stored at `memory`
    */
+      clearMemory: function(x){
+        memory = 0;
+      },
 
   /**
    * Validation
    */
-
+      validation: function(x){
+        }
+    };
+  return calculator;
+}
