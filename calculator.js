b7399b86f92d353c1696d2d63bfc75259073f15d
Lisa <misslisazhou@gmail.com>
2016-05-18 22:07:12 -1000

    pass validations for add function

--- calculator.js
+++ calculator.js
@@ -49,7 +49,11 @@ var calculatorModule = function () {
    * @param { Number } x
    */
     function add(x) {
-      total +=x;
+      if (typeof x !== "number"){
+        throw new Error("Oh oh, an error has occured");
+      }else{
+        total +=x;
+      }
     }
 
 
