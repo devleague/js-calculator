2a187fd3b4899fa232edb1d5b9641447b2556537
sgnl <rayrfarias@gmail.com>
2016-09-19 10:13:51 -1000

    refactors test bootstrap process for cli & browser environments

--- test/calculator_spec.js
+++ test/calculator_spec.js
@@ -1,16 +1,17 @@
 /****Check if Browser or CLI****/
-var window = window || undefined;
+var window = window || false;
+var calculatorModule;
 
-if (window) {
-  GLOBAL = window;
-} else {
-  var fs = require('fs');
-  var vm = require('vm');
+if (!window) {
   var chai = require('chai');
-  var functionsFile = fs.readFileSync(process.cwd() + '/calculator.js');
-  vm.runInThisContext(functionsFile); // file runs and it's contents has access to GLOBAL
+  calculatorModule = require('../calculator.js');
 }
+
+var expect = chai.expect;
+chai.should();
 
 /**
 * write tests below
 */
+
+
