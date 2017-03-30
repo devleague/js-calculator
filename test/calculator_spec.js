6e1e392a5ee5e05b6572cfd2c21fe5fb753da0c0
sgnl <rayrfarias@gmail.com>
2017-03-30 10:47:01 -1000

    adds comments to guide students when writing test for the first time

--- test/calculator_spec.js
+++ test/calculator_spec.js
@@ -1,17 +1,33 @@
-/****Check if Browser or CLI****/
-var window = window || false;
-var calculatorModule;
+// import dependencies e.g. chai and the calculator module
 
-if (!window) {
-  var chai = require('chai');
-  calculatorModule = require('../calculator.js');
-}
+// define two variables: `expect` and `should`.
 
-var expect = chai.expect;
-chai.should();
+// define a `describe` block. what are we testing? what should be name this suite? This is the "container" describe block.
 
-/**
-* write tests below
-*/
+  /**
+   * each method should have it's own `describe` block
+   */
 
+  // define a `describe` block to test the `load` method and all these tests go INSIDE of this describe
 
+  // define a `describe` block to test the `getTotal` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `add` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `subtract` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `multiply` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `divide` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `recallMemory` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `saveMemory` method and all these tests go INSIDE of this describe
+
+  // define a `describe` block to test the `clearMemory` method and all these tests go INSIDE of this describe
+
+  /**
+   * FINAL BOSS
+   */
+
+  // define a `describe` block to test the `validation` features of your methods and all these tests go INSIDE of this describe
