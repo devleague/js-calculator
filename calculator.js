e4d675f2c4c5b87e4eb7692c1ee24b4d9a31bc5c
Mariel Salcedo <Mariel Salcedo>
2017-06-14 19:10:51 -1000

    finished

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 /**
  * Declare a function named `calculatorModule`
  * this function will have two private variables declared inside of it.
@@ -5,7 +6,10 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+function calculatorModule (){
 
+  let memory = 0;
+  let total = 0;
 
   /**
    * sets the `total` to the number passed in
@@ -13,53 +17,113 @@
    * @return { Number }    current total
    */
 
+   function load(x){
+
+     validation(x);
+     total = x;
+     return total;
+   }
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+   function getTotal(x){
+      return total;
+   }
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
+   function add(x){
 
+      validation(x);
+      total += x;
+      return total;
+   }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+   function subtract(x){
 
+      validation(x);
+      total -= x;
+      return total;
+  }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
+   function multiply(x){
 
+      validation(x);
+      total *= x;
+      return total;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   function divide(x){
+
+      validation(x);
+      total /= x;
+      return total;
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   function recallMemory(){
 
+      return memory;
+   }
   /**
    * Stores the value of `total` to `memory`
    */
 
+   function saveMemory(){
+
+      memory = total;
+   }
 
   /**
    * Clear the value stored at `memory`
    */
+   function clearMemory(){
+
+      memory = 0;
+   }
 
   /**
    * Validation
    */
+    function validation(x){
+    if (typeof x !== 'number'){
+      throw new Error("Error");
 
+      }
+    }
+
+
+   return {
+      load: load,
+      getTotal: getTotal,
+      add: add,
+      subtract: subtract,
+      multiply: multiply,
+      divide: divide,
+      recallMemory: recallMemory,
+      saveMemory: saveMemory,
+      clearMemory: clearMemory,
+    };
+
+}
