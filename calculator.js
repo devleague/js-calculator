a88e0d091f210ce5bb9399c8b209fe15c4abdfaa
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2017-12-27 19:15:04 -1000

    Finished

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+
 /**
  * Declare a function named `calculatorModule`
  * this function will have two private variables declared inside of it.
@@ -5,61 +6,103 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
-
+function calculatorModule() {
+   var memory = 0;
+   var total = 0;
+ return calculator = {   
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
-
-  /**
+load:  (x) => {
+    if (typeof x === 'number') {
+        total = x;
+        return x;
+    } else {
+        throw new Error;
+    }
+},
+/**
    * Return the value of `total`
    * @return { Number }
    */
 
-
+getTotal:() => {
+    return total;
+},
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+add: (x) => {
+    if (typeof x === 'number') {
+        total += x;
+        return total;
+    } else {
+        throw new Error;
+        
+    }
+},
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+subtract: (x) => {
+    if (typeof x === 'number') {
+        total -= x;     
+    } else {
+        throw new Error;
+    }
+},
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
-
+multiply:  (x) => {
+    if (typeof x === 'number') {
+        total *= x;
+    } else {
+        throw new error;
+    }
+},
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
-   */
-
-
-  /**
+   */ 
+divide:  (x) => {
+    if (typeof x === 'number') {
+        total /= x;     
+    } else {
+        throw new error;
+    }
+},
+/**
    * Return the value stored at `memory`
    * @return { Number }
-   */
-
-
-  /**
+   */  
+recallMemory:  () => {
+    return memory;
+},
+ /**
    * Stores the value of `total` to `memory`
    */
-
-
-  /**
+saveMemory:  () => {
+    memory = total;
+},
+ /**
    * Clear the value stored at `memory`
    */
-
-  /**
+clearMemory: () => {
+    memory = 0;
+},
+ /**
    * Validation
    */
-
+}
+}
